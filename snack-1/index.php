0cdeee0f292158ad13842411cc2813cdd78b96a8
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-21 11:47:16 +0200

    snack-1

---
+++ snack-1/index.php
@@ -0,0 +1,34 @@
+<?php 
+/* Creare un array contenente qualche alunno di un'ipotetica classe. 
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Stampare Nome, Cognome e la media dei voti di ogni alunno. */
+
+$students = [
+    [
+        'name' => 'Laura',
+        'lastname' => 'Bianchi',
+        'votes' => [9, 9, 9, 9, 10]
+    ],
+    [
+        'name' => 'Michele',
+        'lastname' => 'Papagni',
+        'votes' => [8, 5, 7, 6]
+    ],
+    [
+        'name' => 'Fabio',
+        'lastname' => 'Forghieri',
+        'votes' => [10, 2, 4]
+    ],
+    [
+        'name' => 'Roberto',
+        'lastname' => 'Marazzini',
+        'votes' => [2, 8, 3, 4, 5]
+    ],
+    [
+        'name' => 'Roberta',
+        'lastname' => 'Danderini',
+        'votes' => [2, 8, 9, 5, 4]
+    ]
+];
+
+?>
