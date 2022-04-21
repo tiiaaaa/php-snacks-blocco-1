bcbdd19eda2da001439ed280678e1fa03005f154
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-21 18:07:41 +0200

    correzione snack-1

--- snack-1/index.php
+++ snack-1/index.php
@@ -32,3 +32,26 @@ $students = [
 ];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snak1</title>
+</head>
+<body>
+    <div>
+        <ul>
+            <?php foreach($students as $student) {?>
+                <li>
+                    <?php echo $student['name'] . ' ' . $student['lastname'] . ' - ';?>
+                    <?php echo array_sum($student['votes']) /  count($student['votes']); ?>
+                </li>
+                
+            <?php } ?>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ snack-3/index.php
@@ -0,0 +1,37 @@
+<?php 
+
+/* Partendo da questa stringa $string = "c,i,n,q,u,e"; 
+trasforma la stringa in un array con un elemento per carattere,
+
+	$array = ['c','i','n','q','u','e'];
+
+infine crea un array associativo della forma:
+	
+	$result = [
+		'c' => 'c',
+		'i' => 'i',
+		'n' => 'n',
+		'q' => 'q',
+		'u' => 'u',
+		'e' => 'e'
+	]; */
+
+    $string = "c,i,n,q,u,e";
+
+    $array = str_split($string, 1);
+    var_dump($array);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-3</title>
+</head>
+<body>
+    
+</body>
+</html>
